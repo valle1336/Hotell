70eedd2f0ffb83e1fb548986e2f21a12ec5fe0a3
valle1336 <12alparholoskolan.se>
2023-01-12 00:37:48 +0100

    Uppdaterat menyn och fixat javascript

--- hotell.js
+++ hotell.js
@@ -2,6 +2,9 @@ const meny = document.getElementById("Meny-js");
 
 const knapp = document.getElementById("menyButton");
 
+let screenWidth = screen.width;
+
+
 function hideMeny() {
     meny.classList.toggle("hide");
     console.log("Hide funtion used.");
@@ -9,9 +12,30 @@ function hideMeny() {
 
 knapp.addEventListener("click", hideMeny);
 
+if(screenWidth < 1000) {
+    meny.classList.toggle("hide");
+}
+
+if(screenWidth >= 1000) {
+    knapp.classList.toggle("hide");
+}
+
 const kontaktMeny = document.getElementById("Kontakt-js");
 
 const kontaktKnapp = document.getElementById("Skicka-js");
+
+
+function hideKontakt() {
+    kontaktMeny.classList.toggle("hideKontakt");
+    console.log("Kontakt hide funtion used.");
+}
+
+kontaktKnapp.addEventListener("click", hideKontakt);
+
+
+
+
+
 
 
 
